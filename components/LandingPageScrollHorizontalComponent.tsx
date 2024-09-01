6a08771edc1ddcd7f11c09f0ddb0d2516bdb96de
Kundan Kumar Sahu <espkundan@gmail.com>
2024-09-01 23:43:01 +0530

    testing transation from wallet

--- components/LandingPageScrollHorizontalComponent.tsx
+++ components/LandingPageScrollHorizontalComponent.tsx
@@ -1,28 +1,20 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
-import { Badge } from "@/components/ui/badge";
 import { Star, Eye } from "lucide-react";
 import { WebToonInterface } from "@/testdata/cardP";
 import { krabbyPatty } from "@/lib/fonts/font";
-import {
-  Asset,
-  Aurora,
-  BASE_FEE,
-  Keypair,
-  Networks,
-  Operation,
-  TransactionBuilder,
-} from "diamnet-sdk";
-import { auroraServerUrl, masterSecret } from "../constants/constants";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import Loader from "@/components/Loader/Loader"; // Assuming you have a loader component
+import { Aurora, BASE_FEE, Keypair, Networks, Operation, TransactionBuilder } from "diamnet-sdk";
+import { auroraServerUrl, masterSecret } from "@/constants/constants";
 
 export const WebtoonScrollComponent = ({ title }: { title: string }) => {
   const [webToons, setWebToons] = useState<WebToonInterface[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [transactionLoading, setTransactionLoading] = useState<boolean>(false);
+  const router = useRouter();
 
   useEffect(() => {
     const fetchWebToons = async () => {
@@ -47,23 +39,37 @@ export const WebtoonScrollComponent = ({ title }: { title: string }) => {
     fetchWebToons();
   }, []);
 
-  const handleMangaPurchase = async (webToon: WebToonInterface) => {
-    if (!window.diam) {
-      toast.error("DIAM wallet is not connected");
-      return;
-    }
-
+  const handleWebtoonClick = async (webToon: WebToonInterface) => {
+    setTransactionLoading(true);
     try {
-      const connectionResp = await window.diam.connect();
-      const userAddress = connectionResp.message[0].diamPublicKey;
+      const userAddress = await connectToDiamWallet();
+      const transactionSuccess = await initiateTransaction(userAddress);
 
+      if (transactionSuccess) {
+        router.push(`/blocktoon/${webToon._id}`);
+      } else {
+        alert("Transaction failed, please try again.");
+      }
+    } catch (err) {
+      console.error("Transaction error:", err);
+      alert("An error occurred during the transaction.");
+    } finally {
+      setTransactionLoading(false);
+    }
+  };
+
+  const connectToDiamWallet = async () => {
+    const connectionResp = await window.diam.connect();
+    return connectionResp.message[0].diamPublicKey;
+  };
+
+  const initiateTransaction = async (userAddress: string) => {
+    try {
       const masterKeypair = Keypair.fromSecret(masterSecret);
       const issuerKeypair = Keypair.random();
       const server = new Aurora.Server(auroraServerUrl);
-
       const masterAccount = await server.loadAccount(masterKeypair.publicKey());
-
-      const asset = new Asset(webToon.title, issuerKeypair.publicKey());
+      const asset = new Asset("MangaAccess", issuerKeypair.publicKey());
 
       const tx = new TransactionBuilder(masterAccount, {
         fee: BASE_FEE,
@@ -101,20 +107,12 @@ export const WebtoonScrollComponent = ({ title }: { title: string }) => {
       tx.sign(masterKeypair, issuerKeypair);
 
       const xdr = tx.toXDR();
-      const signResponse = await window.diam.sign(
-        xdr,
-        true,
-        Networks.TESTNET
-      );
+      const signResponse = await window.diam.sign(xdr, true, Networks.TESTNET);
 
-      if (signResponse.response.status === 200) {
-        toast.success("Transaction Successful");
-      } else {
-        console.log(signResponse);
-        toast.error("Transaction Failed");
-      }
-    } catch (error) {
-      toast.error("Transaction Failed");
+      return signResponse.response.status === 200;
+    } catch (e) {
+      console.error("Transaction initiation failed:", e);
+      return false;
     }
   };
 
@@ -127,41 +125,40 @@ export const WebtoonScrollComponent = ({ title }: { title: string }) => {
       <ScrollArea className="w-[98vw] whitespace-nowrap rounded-md border">
         <div className="flex w-max space-x-4 p-4">
           {webToons.map((webToon: WebToonInterface) => (
-            <div key={webToon._id} onClick={() => handleMangaPurchase(webToon)}>
-              <Link href={`/blocktoon/${webToon._id}`}>
-                <div className="w-[250px] space-y-3">
-                  <div className="overflow-hidden rounded-md">
-                    <img
-                      src={webToon.coverImage}
-                      alt={webToon.title}
-                      className="h-auto w-auto object-cover transition-all hover:scale-105 aspect-[3/4]"
-                    />
+            <div
+              key={webToon._id}
+              className="w-[250px] space-y-3 cursor-pointer"
+              onClick={() => handleWebtoonClick(webToon)}
+            >
+              <div className="overflow-hidden rounded-md">
+                <img
+                  src={webToon.coverImage}
+                  alt={webToon.title}
+                  className="h-auto w-auto object-cover transition-all hover:scale-105 aspect-[3/4]"
+                />
+              </div>
+              <div className="space-y-1 text-sm">
+                <h3 className="font-medium leading-none">{webToon.title}</h3>
+                <p className="text-xs text-muted-foreground">
+                  {webToon.author}
+                </p>
+                <div className="flex gap-2">
+                  <div className="flex items-center gap-1">
+                    <Star size={16} />
+                    {webToon.rating.toFixed(1)}
                   </div>
-                  <div className="space-y-1 text-sm">
-                    <h3 className="font-medium leading-none">
-                      {webToon.title}
-                    </h3>
-                    <p className="text-xs text-muted-foreground">
-                      {webToon.author}
-                    </p>
-                    <div className="flex gap-2">
-                      <div className="flex items-center gap-1">
-                        <Star size={16} />
-                        {webToon.rating.toFixed(1)}
-                      </div>
-                      <div className="flex items-center gap-1">
-                        <Eye size={16} />
-                        {webToon.viewCount}
-                      </div>
-                    </div>
+                  <div className="flex items-center gap-1">
+                    <Eye size={16} />
+                    {webToon.viewCount}
                   </div>
                 </div>
-              </Link>
+              </div>
             </div>
           ))}
         </div>
         <ScrollBar orientation="horizontal" />
       </ScrollArea>
+      {transactionLoading && <Loader />} {/* Show loading spinner during transaction */}
     </div>
   );
 };
